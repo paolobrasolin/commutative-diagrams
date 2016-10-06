266fc9d0f79d60546f61cf0542888b74a97c74c0
Travis CI <paolo.brasolin@gmail.com>
2016-10-06 17:42:16 +0000

    Deploy to GitHub Pages: df06089abd29461f588e1717b9ccf86e3bc408fd

--- js/live-demo.js
+++ js/live-demo.js
@@ -8,7 +8,7 @@ function getNaturalWidth(obj, text){
 }
 
 
-$('p').hyphenate('en-gb');
+// $('p').hyphenate('en-gb');
 
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/chrome");
@@ -24,52 +24,82 @@ editor.on('change', function() {
     .html("Compile!");
 });
 
+var logger = ace.edit("logger");
+// logger.setTheme("ace/theme/chrome");
+// logger.getSession().setMode("ace/mode/latex");
+logger.setOptions({
+  readOnly: true,
+  showGutter: false,
+  fontSize: 16,
+  maxLines: Infinity,
+  // minLines: 2
+});
 
 $('#compile').click(function() {
 
-  $('#display').transition({ 'min-height': '6em' });
+  $('#diagram-wrapper').transition({ opacity: 0 });
+  $('#logger-wrapper').transition({ opacity: 0 });
+  $('#loader-wrapper').transition({ opacity: 1 });
 
-  $('.diagram').transition({ opacity: 0 });
-  $('.loader').transition({ opacity: 1 });
+  $('#display')
+      .transition({ height: $('#loader-wrapper').css('height') }, 150 );
 
   $('#compile')
     .attr('disabled', true)
     .html("Compiling&hellip;");
 
   var longWaitTimerId = setTimeout(function () {
-    $('.loader-message')
+    $('#loader-message')
       .html("Server asleep: waking up the poor thing.");
   }, 2400);
 
+  $('#loader-message').html('');
+
   $.ajax({
+
     method: 'POST',
     url: 'https://texrhobot.herokuapp.com/crank',
-    // url: 'http://0.0.0.0:5000/svg',
+    // url: 'http://0.0.0.0:9292/crank',
     data: {
-      "document": editor.getValue()
+      "document": editor.getValue(),
+      'template': 'latex-minimal',
+      'format': 'kodi'
     }
-  }).done(function(data) {
-    clearTimeout(longWaitTimerId);
-    var svg = $(data).find("svg");
-    var exHeight = parseFloat(svg.attr('height'))/parseFloat("4.30554pt")+"ex";
-    var exWidth = parseFloat(svg.attr('width'))/parseFloat("4.30554pt")+"ex";
-    svg.attr('height', exHeight);
-    svg.attr('width', exWidth);
 
-    $('.diagram')
-      .transition({ height: exHeight, duration: 150 })
+  }).done(function(data) {
+
+    $('#loader-wrapper').transition({ opacity: 0 });
+    $('#compile').html("Tweak the code!");
+    clearTimeout(longWaitTimerId);
+
+    var svg = $(data).find("svg");
+
+    var exHeight = parseFloat(svg.attr('height'))/4.3+"ex";
+    var exWidth  = parseFloat(svg.attr('width' ))/4.3+"ex";
+    svg.attr('height', exHeight);
+    svg.attr('width',  exWidth );
+    $('#display')
+      .transition({ height: exHeight }, 150 );
+
+    $('#diagram-wrapper')
       .html(svg)
       .transition({ opacity: 1 },  150, 'snap' );
-    $('.loader').transition({ opacity: 0 });
 
-    $('.loader-message').html('');
-    $('#compile')
-      .html("Tweak the code!");
+  }).fail(function(jqXHR, textStatus, errorThrown) {
 
+    $('#loader-wrapper').transition({ opacity: 0 });
+    $('#compile').html("You goofed up.");
 
-  }).fail(function(err) {
-    console.log( err );
+    logger.setValue(jqXHR.responseText, -1);
+    $('#logger-wrapper').transition({ opacity: 1 });
+
+    /* TODO: this is ridiculous, dammit... */
+    setTimeout(function () {
+      var h = $('#logger-wrapper').css('height');
+      $('#display')
+        .transition({ height: h }, 140 );
+    }, 10);
+
   });
-
 
 });
