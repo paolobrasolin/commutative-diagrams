2ce683041a0462abdb9483cc511a024005d55735
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-02-04 17:06:40 +0100

    refactored mitra and ozos sourcecode

---
+++ src/auto/tikzlibrarykodi.ozos.code.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "tikzlibrarykodi.ozos.code"
+ (lambda ()
+   (TeX-add-symbols
+    "kDOzos"
+    "kDOzosFetchThen"
+    "kDOzosMaybeDumpThen"
+    "kDOzosOutput"))
+ :plain-tex)
+
